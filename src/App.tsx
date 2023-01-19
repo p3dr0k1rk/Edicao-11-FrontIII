5238666f3c79a74fa2275553261bcecb12a97aee
aglayrton <aglayrtonjuliao@gmail.com>
2023-01-18 21:53:02 -0300

    View with initialState and Actions/dispach

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import CssBaseline from '@mui/material/CssBaseline';
-import MuiLayout from './components/MuiLayout';
-import MuiTextField from './components/MuiTextField';
-import HookTextField from './hooksmui/MuiTextField';
-import Home from './page/Home';
-import { ThemeProvider } from '@mui/material';
-import { bob, sponja } from './theme/theme';
-
-//criar um temta
-
+import { RootState } from './store/store';
+import { useDispatch, useSelector } from 'react-redux/es/exports';
+import { incrementar, decrementar } from './store/feature/contadorSlice';
 function App() {
+	//a partir dele, tu vai disparar as actions
+	const dispatch = useDispatch();
+	//pegar o estado inicial
+	const initialState = useSelector((state: RootState) => state.contadorReducer);
 	return (
-		<ThemeProvider theme={bob}>
-			<CssBaseline />
-			{/* <MuiLayout />
-      <MuiTextField/> */}
-			{/* <HookTextField /> */}
-			<Home />
-		</ThemeProvider>
+		<>
+			<h1>{initialState.conta}</h1>
+			<button onClick={()=>dispatch(incrementar())}>incrementar</button>
+			<button onClick={()=>dispatch(decrementar())}>decrementar</button>
+		</>
 	);
 }
-
 export default App;
