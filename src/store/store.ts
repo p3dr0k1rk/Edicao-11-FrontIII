a00704db58a014dc77ad72f173ce83bab02585bb
aglayrton <aglayrtonjuliao@gmail.com>
2023-01-18 21:52:22 -0300

    Store with reducers/slices

---
+++ src/store/store.ts
@@ -0,0 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit';
+import contadorSlice from './feature/contadorSlice';
+const store = configureStore({
+	reducer: {
+		//o meu slice/reducer
+		contadorReducer: contadorSlice,
+	},
+});
+//TYPESCRIPT
+//exportando o estado inicial
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export default store;
+
