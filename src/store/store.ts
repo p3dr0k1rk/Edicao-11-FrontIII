d2d2b81010e07daf01d9dd66ecc2f728bbe37d8d
aglayrton <aglayrtonjuliao@gmail.com>
2023-01-23 20:51:29 -0300

    Adicionado uma reducer/slice

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import contadorSlice from './feature/contadorSlice';
+import produtoSlice from './feature/produtoSlice';
 const store = configureStore({
 	reducer: {
 		contadorReducer: contadorSlice,
+		produtosReducer: produtoSlice
 	},
 });
 //TYPESCRIPT
 //exportando o estado inicial
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
 export default store;
 
