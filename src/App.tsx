e2a7e7af764a3473234940140b7d548148d94ed1
aglayrton <aglayrtonjuliao@gmail.com>
2023-01-23 20:52:10 -0300

    Chamando o componente Produto

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { RootState } from './store/store';
 import { useDispatch, useSelector } from 'react-redux/es/exports';
 import { incrementar, decrementar } from './store/feature/contadorSlice';
+import Produto from './page/Produto';
 function App() {
 	const [valor, setValor] = useState(0);
 	const dispatch = useDispatch();
@@ -16,6 +17,7 @@ function App() {
 			<h1>{initialState.conta}</h1>
 			<button onClick={() => dispatch(incrementar(valor))}>incrementar</button>
 			<button onClick={() => dispatch(decrementar())}>decrementar</button>
+			<Produto/>
 		</>
 	);
 }
